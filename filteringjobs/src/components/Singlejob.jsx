f16ca3f715a8529c9acf6780706218cd35dabc1b
Sammynauta <samuka.o.diogo@gmail.com>
2021-09-05 15:47:07 -0300

    finalizando projeto, ainda falta estilizar componentes.

--- filteringjobs/src/components/Singlejob.jsx
+++ filteringjobs/src/components/Singlejob.jsx
@@ -32,13 +32,13 @@ const SingleJob = (props) => {
   }, [logo]);
 
   return (
-    <div className="job-container">
+    <div className={ featured ? 'job-container job-container--borderleft' : 'job-container'}>
       <div className="logo">
         <img src={icon} alt="imagem" />
       </div>
       <div className="part1">
         <div className="company">
-          <div className="cname">{company}</div>
+          <span className="cname">{company}</span>
           {props.data.new && <span className="new">new!</span>}
           {props.data.featured && <span className="featured">featured</span>}
         </div>
@@ -46,15 +46,18 @@ const SingleJob = (props) => {
 
         <div className="details">
           <span>{postedAt}</span>
+          <span>&nbsp;•&nbsp;</span>
           <span>{contract}</span>
+          <span>&nbsp;•&nbsp;</span>
           <span>{location}</span>
         </div>
       </div>
+      
       <div className="part2">
         {keywords.map((key, id) => (
-          <button onClick={() => props.setKeywords(key)} key={id}>
+          <span onClick={() => props.setKeywords(key)} key={id}>
             {key}
-          </button>
+          </span>
         ))}
       </div>
     </div>
